0e83a9e8fe34ea468d52e21e571ca56df2372c00
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-02 12:14:41 +0600

    fixing strictQuery error

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
+		mongoose.set("strictQuery", false);
+
 		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
