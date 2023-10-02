efb0e1224b27aa0776ac08cd22cae558105ab794
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-02 14:14:57 +0600

    .

--- frontend/src/frontEndConfig.js
+++ frontend/src/frontEndConfig.js
@@ -1,6 +1,6 @@
 const config = {
-	// SERVER_DOMAIN: "http://localhost:8000",
-	SERVER_DOMAIN: "https://backend-cityshop-fyxs.onrender.com/",
+	SERVER_DOMAIN: "http://localhost:8000",
+	// SERVER_DOMAIN: "https://backend-cityshop-fyxs.onrender.com/",
 	STRIPE_PUBLIC_KEY:
 		"pk_test_51NwMokExTKwmYDjHR3E5upuF2LRmYQwKUjf4RwDvyh31pmwEkc40KaT0D4J0EDQigjN2NYwzoBXfNZbEGNCBSM200020myYDeF",
 };
